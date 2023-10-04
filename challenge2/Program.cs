a18136880d58290d4c5eb4827f3e188457252766
tuananh582 <101263575+tuananh582@users.noreply.github.com>
2023-10-05 00:47:55 +0700

    new

---
+++ challenge2/Program.cs
@@ -0,0 +1,14 @@
+﻿using Internal;
+// See https://aka.ms/new-console-template for more information
+using System;
+class Progam{
+    static void Main(string[] args){
+        Console.WriteLine("Input a string in line : ");
+        string input=Console.ReadLine();
+         
+        
+
+
+
+    }
+}
---
+++ challenging2/challenging2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace challenging2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a string in line");
+            string input = Console.ReadLine();
+            Console.WriteLine("Input a character in line");
+            char inputchar = Console.ReadLine()[0];
+            int indexchar = input.IndexOf(inputchar);
+            Console.WriteLine($"we have a string {input} and the charater we typed is {inputchar} and the position its {indexchar} ");
+            Console.ReadKey();
+
+        }
+    }
+
+}
+
+
